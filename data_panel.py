7ddd5ab5e234eed9c93d1ce0ea9a03e6a8edc3a6
Szymon Hudziak <szymonh.dg@gmail.com>
2023-10-29 20:41:26 +0100

    add simple gui to display changing variable

--- data_panel.py
+++ data_panel.py
@@ -1,10 +1,35 @@
-import tkinter as tk
+import pygame
+import time
 
-sensor_value =300.4 #said sensor value
-window = tk.Tk()
-x = sensor_value #assigned to variable x like you showed
-window.minsize(width=400, height=400)
-w = tk.Label(window, text=x) #shows as text in the window
-w.pack() #organizes widgets in blocks before placing them in the parent.
+# Initializing Pygame
+pygame.init()
 
-tk.mainloop()
+# Initializing surface
+screen = pygame.display.set_mode((400, 300))
+
+# Initializing Color
+color = (0, 0, 0)
+
+# Drawing Rectangle
+# pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+
+
+fontTitle = pygame.font.SysFont("arial", 20)
+
+pygame.display.flip()
+
+x = 0
+
+while True:
+    textTitle = fontTitle.render(f"Iteration: {x}", True, color)
+    screen.fill((255, 255, 255))
+    screen.blit(textTitle, (150, 200))
+    pygame.display.flip()
+
+    x = x + 1
+    time.sleep(1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
